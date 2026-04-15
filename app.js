7e5ba2e45af1dc8ad43034a16bad2602fb8c81b9
Yash Sharma <yshsharma90913@gmail.com>
2026-04-16 01:45:28 +0530

    setup EJS templating and reusable navbar with express routing , currently on home page

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+app.get('/home',(req,res)=>{
+    res.render('home');
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about');
+})
+
+app.get('/alternative',(req,res)=>{
+    res.render('alternative');
+})
+
+app.get('/awareness',(req,res)=>{
+    res.render('awareness');
+})
+
+app.get('/result',(req,res)=>{
+    res.render('result');
+})
+
+app.get('/insights',(req,res)=>{
+    res.render('insights');
+})
+
+
+
+
+app.listen(3001,()=>{
+    console.log("Server is running on port 3001");
+})
