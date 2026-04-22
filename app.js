6b2f0d2d8d8cd3c40bae26f9e487eb3da0d62d7a
Yash Sharma <yshsharma90913@gmail.com>
2026-04-22 21:52:51 +0530

    Home page moved to ejs file from normal html file

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 app.use(express.static('public'));
 app.set('view engine','ejs');
 
-app.get('/',(req,res)=>{
-    res.render('index');
-})
+// app.get('/',(req,res)=>{
+//     res.render('index');
+// })
 
-app.get('/home',(req,res)=>{
+app.get('/',(req,res)=>{
     res.render('home');
 })
 
